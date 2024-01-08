90cef3e58831551547a08ac6bd629f0002fb1cfd
Yuka Ishida <ishida.yuka@valuence.inc>
2024-01-09 08:15:59 +0900

    29. TaskCardTitleコンポーネントを作成しよう

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    console.log(inputCardTitle)
+    setInputCardTitle(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  }
+
   return (
-    <div>
-      
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
     </div>
-  )
-}
+  );
+};

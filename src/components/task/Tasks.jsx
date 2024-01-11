7f03e20878d9f9a7f595b8a111343d0a8a56a2f2
Yuka Ishida <ishida.yuka@valuence.inc>
2024-01-12 08:54:44 +0900

    38. ドラッグ＆ドロップ後にカードを並び替えよう

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -2,16 +2,27 @@ import React from "react";
 import { Task } from "./Task";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
+const reorder = (list, startIndex, endIndex) => {
+      // タスクを並び替える
+      const remove = list.splice(startIndex, 1);
+      list.splice(endIndex, 0, remove[0]);
+}
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  };
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task) => (
+              {taskList.map((task, index) => (
                 <div key={task.id}>
                   <Task
+                    index={index}
                     task={task}
                     taskList={taskList}
                     setTaskList={setTaskList}
